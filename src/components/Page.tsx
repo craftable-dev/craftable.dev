e118a11fc011a8ff83388f853884a050bf7da443
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2022-12-23 09:27:08 -0500

    Removing next-seo

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,6 @@
-import { NextSeo } from "next-seo"
+import Favicons from 'components/Favicons'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 interface Props {
   children: React.ReactNode
@@ -6,19 +8,40 @@ interface Props {
   title: string
 }
 
+const devMode = process.env.NODE_ENV === 'development'
+
 export default function Page(props: Props) {
-  const devMode = process.env.NODE_ENV === 'development'
+  const router = useRouter()
   const { children, description, title } = props
 
   return (
     <>
       {devMode && <div className="debug-screens" />}
       <div />
-      <NextSeo
-        title={title}
-        description={description}
-        openGraph={{ title, description, }}
-      />
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="https://craftable.dev/assets/og.png" />
+        <meta property="og:site_name" content="Craftable" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content="https://craftable.dev/assets/og.png"
+        />
+        <meta property="og:image:alt" content="Craftable Logo" />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="500" />
+        <link
+          rel="canonical"
+          href={`https://craftable.dev${router.pathname}`}
+        />
+        <Favicons />
+      </Head>
       <main>{children}</main>
     </>
   )
--- src/pages/_app.route.tsx
+++ src/pages/_app.route.tsx
@@ -1,22 +1,5 @@
 import 'styles/main.css'
-import Head from 'next/head'
-import { DefaultSeo } from "next-seo"
-import SEO from "../../next-seo.config"
-import Favicons from 'components/Favicons'
 
 export default function MyApp({ Component, pageProps }: any) {
-  return (
-    <>
-      <DefaultSeo {...SEO} />
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        />
-        <Favicons />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
